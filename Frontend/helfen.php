c42efb17e8aad1c04d1caa24952fb5b494c03200
Timon Masberg <kontakt@timonmasberg.de>
2016-10-15 21:51:50 +0200

    Bisschen Code

--- Frontend/helfen.php
+++ Frontend/helfen.php
@@ -1,5 +1,5 @@
 <?php
- include '../backend/db.php';
+include '../backend/db.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -23,46 +23,71 @@ error_reporting(E_ALL);
         <div class="page-header"><h1>Helping-Box<small> Denen helfen, die Hilfe brauchen</small></h1></div>
 
         <div class="container-fluid">
-            <form method="post" action="/backend/redirect.php" id="redirect">
-                <input type='hidden' name='userlat' value=''>
-                <input type='hidden' name='userlong' value=''>
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <nav class="navbar navbar-default" role="navigation">
-                            <div class="navbar-header">
 
-                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapsediv">
-                                    <span class="sr-only">Navigation </span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                                </button> <a class="navbar-brand" href="index.html">HelpingBox</a>
-                            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-default" role="navigation">
+                        <div class="navbar-header">
 
-                            <div class="collapse navbar-collapse" id="collapsediv">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapsediv">
+                                <span class="sr-only">Navigation </span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                            </button> <a class="navbar-brand" href="index.html">HelpingBox</a>
+                        </div>
 
+                        <div class="collapse navbar-collapse" id="collapsediv">
+                            <form method="post" action="helfen.php" id="redirect">
+                                <input type='hidden' id='userlat' name='userlat'>
+                                <input type='hidden' id="userlong" name='userlong'>
                                 <ul class="nav navbar-nav">
                                     <li class="active">
-                                        <a name="helfen" href="javascript:redirect()"  >Helfer</a>
+
+                                        <a name="helfen" href="javascript:redirectHelfer();">Helfer</a>
+
                                     </li>
                                     <li>
-                                        <a name="orga" onclick="redirect()" >Organisation</a>
+                                        <a name="orga" href='orga.php' >Organisation</a>
                                     </li>
                                 </ul>                           
-
-                            </div>
-                        </nav>
-                    </div>
+                            </form>
+                        </div>
+                    </nav>
                 </div>
-            </form>
+            </div>
+
             <h3>Nach Organisationen suchen:</h3></br>
             <div class="list-group">
                 <?php
-      
+
+                function getDistance($from, $to) {
+
+                    $from = "sr nagar,hyderabad";
+                    $to = "kukatpalle,hyderabad";
+                    $from = urlencode($from);
+                    $to = urlencode($to);
+                    $data = file_get_contents("http://maps.googleapis.com/maps/api/distancematrix/json?origins=$from&destinations=$to&language=en-EN&sensor=false");
+                    $data = json_decode($data);
+                    $time = 0;
+                    $distance = 0;
+                    foreach ($data->rows[0]->elements as $road) {
+                        $time += $road->duration->value;
+                        $distance += $road->distance->value;
+                    }
+                    echo "To: " . $data->destination_addresses[0];
+                    echo "<br/>";
+                    echo "From: " . $data->origin_addresses[0];
+                    echo "<br/>";
+                    echo "Time: " . $time . " seconds";
+                    echo "<br/>";
+                    echo "Distance: " . $distance . " meters";
+                }
+
                 $query = "SELECT id, name, adresse FROM organisation";
                 if ($result = $con->query($query)) {
 
                     while ($row = $result->fetch_assoc()) {
                         echo ' <a href="organisation.php?id=' . $row["id"] . '" class="list-group-item">
-                               <h4 class="list-group-item-heading">' . $row["name"] . '</h4>
+                               <h4 class="list-group-item-heading">' . $row["name"] . '<p class="text-right">' . distance(getCoordinates($row["adresse"])[0], getCoordinates($row["adresse"])[1], $_POST["userlat"], $_POST["userlong"]) . ' km entfernt</p></h4>
                     <p class="list-group-item-text">' . $row["adresse"] . '</p>
                                </a>';
                     }
@@ -70,14 +95,11 @@ error_reporting(E_ALL);
                     echo "Im Moment hat sich keine Organisation eingetragen";
                 }
 
-
                 $con->close();
                 ?>
 
             </div>
-            <?php
-            echo $_POST["userlat"];
-            ?>
+
         </div>
     </body>
 </html>
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,14 +1,15 @@
 window.onload = function () {
     navigator.geolocation.getCurrentPosition(function (location) {
-        window.document.getElementsByName("userlat").value = location.coords.latitude;
-        window.document.getElementsByName("userlong").value = location.coords.longitude;
+
+        window.document.getElementById("userlat").value = location.coords.latitude;
+        window.document.getElementById("userlong").value = location.coords.longitude;
     });
 };
 
-function redirect(){
+function redirectHelfer(){
     navigator.geolocation.getCurrentPosition(function (location) {
-        window.document.getElementsByName("userlat").value = location.coords.latitude;
-        window.document.getElementsByName("userlong").value = location.coords.longitude;
+        window.document.getElementById("userlat").value = location.coords.latitude;
+        window.document.getElementById("userlong").value = location.coords.longitude;
         $( "#redirect" ).submit();
     });
     
