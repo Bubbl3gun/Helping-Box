53c7bc98f1bfe3081d14315bbf51daa592c71434
Timon Masberg <kontakt@timonmasberg.de>
2016-10-15 17:07:43 +0200

    Anpassung der Datein

--- backend/db.php
+++ backend/db.php
@@ -2,10 +2,10 @@
 
 // Datenbankverbindungen
 
-$dbname = "";
-$dbpw = "";
-$dburl = "";
-$dbuser = "";
+$dbname = "db242539x2492164";
+$dbpw = "jugendhackt";
+$dburl = "mysql.webhosting61.1blu.de";
+$dbuser = "s242539_2492164";
 
 // Aufbau der Verbindung (OnError => Dirty Error Message)
 $con = mysql_connect($dburl, $dbuser, $dbpw) or die(mysql_error());
